3340e0a89811b963e7b5fe28c07d00139b736ea4
Hayk Bazoyan <bazoyan13@gmail.com>
2021-07-25 13:59:59 +0400

    feat: add react-router & tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Blog from './components/Blog/Blog'
+import LogIn from './components/LogIn/LogIn'
+import CreatePost from './components/CreatePost/CreatePost'
+
 
 export default function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+            <div>
+                <ul>
+                    <li>
+                        <Link to="/">Blog</Link>
+                    </li>
+                    <li>
+                        <Link to="/login">LogIn</Link>
+                    </li>
+                    <li>
+                        <Link to="/createPost">Create Post</Link>
+                    </li>
+                </ul>
+
+                <hr />
+
+                <Switch>
+                    <Route exact path="/">
+                        <Blog />
+                    </Route>
+                    <Route exact path="/login">
+                        <LogIn />
+                    </Route>
+                    <Route exact path="/createPost">
+                        <CreatePost />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
   );
 }
 
---
+++ src/components/Blog/Blog.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+
+export default function Blog() {
+    return (
+        <div>
+            <p>Blog</p>
+        </div>
+    )
+}
