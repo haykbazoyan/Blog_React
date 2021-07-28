1b7a14f4fa0efe187571390ee72d66332bc8326c
Hayk Bazoyan <bazoyan13@gmail.com>
2021-07-28 14:29:20 +0400

    feat: add styles in create post page

--- src/components/CreatePost/CreatePost.jsx
+++ src/components/CreatePost/CreatePost.jsx
@@ -5,15 +5,14 @@ import styles from './CreatePost.module.css'
 export default function CreatePost () {
     return (
         <div className={`${styles.postBg}`}>  
-            <h1>CreatePost</h1>
-
-            <div className='title'>
-                <form action="/action_page.php" id="usrform">
-                    <Input type="text" name="title" placeholder='Title *' />
-                </form>
-                <br />
-                <Textarea rows="4" cols="50" name="content" placeholder='Content' />
+            <h1 className='ml-5'>CreatePost</h1>
+            <hr className='m-5' />
+            <div className='ml-5'>
+                <Input type="text" name="title" placeholder='Title *' />
+                <Textarea rows="20"  name="content" placeholder='Content' />
             </div>
+            
+            
 
         </div>
     )
--- src/components/Textarea/Textarea.jsx
+++ src/components/Textarea/Textarea.jsx
@@ -4,7 +4,7 @@ export default function Textarea(props) {
       return (
         <div>
           <textarea
-            className='text-gray-800 border-2 border-blue-900 py-2.5 px-6 mb-2 mr-2 rounded leading-tight focus:outline-none'
+            className='text-gray-800 border-2 border-blue-900 w-11/12 py-2.5 px-6 mb-2 mr-2 rounded resize-none leading-tight focus:outline-none'
             type={props.type}
             onChange={props.func}
             placeholder={props.placeholder}
